8c8921f46579ccb54858d11977f55c45879238bd
Shashank Korthiwada <skorthiwada@manh.com>
2020-12-26 20:44:18 +0530

    fix : Changed the Input Type For Leap Year

--- leapYear.js
+++ leapYear.js
@@ -5,16 +5,17 @@ const leapOutput = document.querySelector(".leap-output-div");
 let pattern = /^[0-9]{4}$/;
 leapbtn.addEventListener("click", () => {
   let userBirthYear = leapInput.value;
-  if (pattern.test(userBirthYear)) {
-    if (
-      (0 == userBirthYear % 4 && 0 != userBirthYear % 100) ||
-      0 == userBirthYear % 400
-    ) {
+  let sortedBirthYear = userBirthYear.split("-");
+  let year = Number(sortedBirthYear[0]);
+  let month = Number(sortedBirthYear[1]);
+  let day = Number(sortedBirthYear[2]);
+  if (month > 2 || (month === 2 && day < 30)) {
+    if ((0 == year % 4 && 0 != year % 100) || 0 == year % 400) {
       leapOutput.innerHTML = `<div>You are lucky your birth year is a leap year 😀</div>`;
     } else {
       leapOutput.innerHTML = `<div>Your BirthYear is not a leap year 🙂</div>`;
     }
   } else {
-    leapOutput.innerHTML = `<div>Please Enter a Valid Year 😒</div>`;
+    leapOutput.innerHTML = `<div>Please Enter a Valid Date 😒</div>`;
   }
 });
